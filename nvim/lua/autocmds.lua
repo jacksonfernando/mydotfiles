9ebbdebc5639a52608098540d33c01f119ece51e
jacksonfernando <jacksonfernando51@gmail.com>
2022-10-08 08:58:16 +0700

    [Jackson] add autocmd to delete trailing whitespace

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -17,3 +17,10 @@ vim.api.nvim_create_autocmd("FileType", {
         vim.opt.tabstop = 2
     end,
 })
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        vim.cmd([[%s/\s\+$//e]])
+    end,
+})
