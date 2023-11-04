be955b0dd70ed0cb3ba233b542b1d41f749e436d
jacksonfernando <jacksonfernando51@gmail.com>
2023-11-04 09:27:22 +0700

    delete unnecessary comment

--- nvim/lua/configs/mason.lua
+++ nvim/lua/configs/mason.lua
@@ -1,6 +1,4 @@
 local mason = require("mason")
---local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
 mason.setup({
     ui = {
         icons = {
