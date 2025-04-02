2a79062cafcf7fcaf814cf3c6feea3ef554666c3
jacksonfernando <jacksonfernando51@gmail.com>
2025-04-02 20:20:48 +0700

    add copilot and configure the telescope ui select

--- nvim/lua/plugins/common.lua
+++ nvim/lua/plugins/common.lua
@@ -9,9 +9,20 @@ return {
     },
     {
         "nvim-telescope/telescope.nvim",
-        opts = {
-            file_ignore_patterns = { 'node_modules' }
-        }
+        dependencies = { 'nvim-telescope/telescope-ui-select.nvim' },
+        config = function()
+            require("telescope").setup({
+                defaults = {
+                    file_ignore_patterns = { 'node_modules' }
+                },
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({})
+                    }
+                }
+            })
+            require("telescope").load_extension("ui-select")
+        end
     },
     {
         "akinsho/bufferline.nvim",
